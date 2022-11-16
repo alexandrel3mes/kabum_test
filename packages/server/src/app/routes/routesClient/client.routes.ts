fe49d231d298b3c919cedbd50b6c6d974cf343de
Alexandre <alexandreeduardolemes@gmail.com>
2022-11-16 00:38:49 -0300

    feat: implementa validacao jwt ao cadastrar cliente

--- packages/server/src/app/routes/routesClient/client.routes.ts
+++ packages/server/src/app/routes/routesClient/client.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import CreateClientValidation from '../../validators/Client/CreateClientValidation';
 import { createClientController } from '../../useCases/CreateClient'
+import authorize from '../../middlewares/authorize';
 
 const clientRouter = Router();
 
-clientRouter.post('/', CreateClientValidation, (request, response) => {
+clientRouter.post('/', authorize.auth, CreateClientValidation, (request, response) => {
   return createClientController.handle(request, response);
 })
 
