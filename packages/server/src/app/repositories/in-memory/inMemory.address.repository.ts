f18858158544d3d37b26e2713fb0aaa4a56bf28f
Alexandre <alexandreeduardolemes@gmail.com>
2022-11-17 00:05:53 -0300

    feat: insere teste de edit address

--- packages/server/src/app/repositories/in-memory/inMemory.address.repository.ts
+++ packages/server/src/app/repositories/in-memory/inMemory.address.repository.ts
@@ -22,8 +22,26 @@ export class InMemoryAddressRepository implements IAddressRepository {
   }
 
   async edit(addressId: string, addressPayload: IEditAddressRequestDTO): Promise<void> {
-    throw new Error("Method not implemented.");
+    const finder = this.items.find((item) => {
+      return item.id === addressId
+    })
+
+    if (finder) {
+      const index = this.items.indexOf(finder)
+
+      if (addressPayload.address) this.items[index].address = addressPayload.address
+      if (addressPayload.zipcode) this.items[index].zipcode = addressPayload.zipcode
+      if (addressPayload.number) this.items[index].number = addressPayload.number
+      if (addressPayload.complement) this.items[index].complement = addressPayload.complement
+      if (addressPayload.district) this.items[index].district = addressPayload.district
+      if (addressPayload.city) this.items[index].city = addressPayload.city
+      if (addressPayload.state) this.items[index].state = addressPayload.state
+      if (addressPayload.reference) this.items[index].reference = addressPayload.reference
+    }
+
+
   }
+
   async remove(addressId: string): Promise<void> {
     const address = await this.findById(addressId)
     const filter = this.items.filter((item) => item.id !== address.id)
---
+++ packages/server/src/app/useCases/Address/EditAddress/EditAddress.spec.ts
@@ -0,0 +1,56 @@
+import { InMemoryClientRepository } from '../../../repositories/in-memory/inMemory.client.repository'
+import { describe, expect, it } from 'vitest'
+import { InMemoryAddressRepository } from '../../../repositories/in-memory/inMemory.address.repository'
+import { Client } from '../../../entities/Client'
+import { CreateAddressUseCase } from '../CreateAddress/CreateAddressUseCase'
+import { EditAddressUseCase } from './EditAddressUseCase'
+
+describe('Edit Address', () => {
+  it('should be able to remove an address', async () => {
+    const mockRepo = new InMemoryAddressRepository()
+    const mockClientRepo = new InMemoryClientRepository()
+    const createUseCase = new CreateAddressUseCase(mockRepo, mockClientRepo)
+    const editUseCase = new EditAddressUseCase(mockRepo)
+    const mockPayload = {
+      zipcode: '29060670',
+      address: 'Rua tal',
+      number: '110',
+      complement: 'La do lado',
+      district: 'Bairro X',
+      city: 'Sao Paulo',
+      state: 'SP',
+      reference: 'Perto de não sei aonde',
+    }
+
+    const mockClientPayload = {
+      name: 'Alexandre',
+      cpf: '06577789566',
+      birthday: '16/06/1999',
+      rg: '15889546',
+      phone: '87992456785',
+      addresses: [mockPayload]
+    }
+
+    const mockEditPayload = {
+      district: 'Bairro Y',
+      city: 'Rio Branco',
+      state: 'AC',
+    }
+
+    const clientMock = new Client(mockClientPayload)
+
+    mockClientRepo.items.push(clientMock)
+
+    if (clientMock.id)
+    await createUseCase.execute(clientMock.id, mockPayload)
+
+    const find = mockRepo.items.find((item) => item.zipcode === mockPayload.zipcode)
+
+    if(find && find.id)
+    await editUseCase.execute(find.id, mockEditPayload)
+
+    expect(find?.district).toEqual('Bairro Y')
+    expect(find?.city).toEqual('Rio Branco')
+    expect(find?.state).toEqual('AC')
+  })
+})
